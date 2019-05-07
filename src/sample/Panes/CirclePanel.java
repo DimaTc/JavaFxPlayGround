df7f4233bc0c23baa1d04c0946fe6354367a045b
Dima Tchenak <Dimatch1@gmail.com>
2019-05-07 23:47:11 +0300

    Adjusted the gravitation formula for better visuals. Adjusted the creation formula to "fill" the gaps when creating circles

--- src/sample/Panes/CirclePanel.java
+++ src/sample/Panes/CirclePanel.java
@@ -11,8 +11,8 @@ public class CirclePanel extends InteractivePane {
     private final static int LAYERS = 10;
     private final static int CIRCLE_PER_LAYER = 15;
     private final static int DIST_FROM_CENTER = 80;
-    private final static int MAX_MOVEMENT = 50;
-    private final static int MASS = 500_000; //in grams
+    private final static int MAX_MOVEMENT = 80;
+    private final static int MASS = 1_000_000; //in grams
     private ArrayList<Circle> circles;
 //    private final static int SENSITIVE_DISTANCE = 300;
 
@@ -27,10 +27,11 @@ public class CirclePanel extends InteractivePane {
     }
 
     public void init() {
-        double radius = 2 * DIST_FROM_CENTER * Math.PI / (CIRCLE_PER_LAYER * 2);
+        double radius =  DIST_FROM_CENTER * Math.PI / (CIRCLE_PER_LAYER );
         for (int layer = 0; layer < LAYERS; layer++) {
-            for (int i = 0; i <= CIRCLE_PER_LAYER; i++) {
-                double deg = i / (double) (CIRCLE_PER_LAYER + 1) * 2 * Math.PI;
+            int numBalls = CIRCLE_PER_LAYER * (layer + 1);
+            for (int i = 0; i <= numBalls; i++) {
+                double deg = i / (double) (numBalls + 1) * 2 * Math.PI;
                 Circle c = new Circle(radius, getRandomColor());
                 c.centerXProperty().bind(widthProperty().divide(2)
                         .add(((DIST_FROM_CENTER + 1) * (layer + 1) * Math.cos(deg))));
@@ -48,8 +49,11 @@ public class CirclePanel extends InteractivePane {
         int circleX = (int) circle.getCenterX();
         int circleY = (int) circle.getCenterY();
         double distance = Math.sqrt(Math.pow(circleX - mx, 2) + Math.pow(circleY - my, 2));
-        double force = MASS / Math.pow(distance, 2);    //Calculation of gravity
+        double force = MASS / Math.pow(distance, 1.8);    //Calculation of gravity
         double movement = -(force > MAX_MOVEMENT ? MAX_MOVEMENT : force);
+//        movement = -force;
+
+
         double deg = Math.atan2(circle.getCenterY() - my, circle.getCenterX() - mx);
         circle.translateXProperty().set(movement * Math.cos(deg));
         circle.translateYProperty().set(movement * Math.sin(deg));
